075a4d53c7beb1168e9d120c16ce1e17c210d1c9
Gregory Lindsey <gclindsey@gmail.com>
2020-05-14 21:43:04 -0700

    Add load_db.sh script

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,3 +2,9 @@ CREATE USER postgres;
 ALTER ROLE postgres WITH PASSWORD 'password';
 CREATE DATABASE permits;
 GRANT ALL PRIVELEGES ON DATABASE permits to postgres;
+
+CREATE TABLE permits_raw (
+    "Assessor Book" INT PRIMARY KEY
+    "Assessor Page" INT
+    "Zip Code" INT
+)
