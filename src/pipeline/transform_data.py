892a2c642fcd0b8d1cbd7f1e5c7e2b4d9cde1f47
Gregory Lindsey <gclindsey@gmail.com>
2020-06-01 17:48:40 -0700

    Clean up transform_data.py

--- src/pipeline/transform_data.py
+++ src/pipeline/transform_data.py
@@ -6,8 +6,7 @@ import logging
 from dotenv import load_dotenv, find_dotenv
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'; turn off SettingWithCopyWarning
-import psycopg2 # SQL libraries
-from src.toolkits.sql import connect_db # Import custom sql functions
+
 
 # Concatenate address columns into full_address column
 def create_column_full_address(data):
@@ -31,11 +30,11 @@ def create_column_full_address(data):
     return data
 
 
-
 def split_lat_long(data):
     
     # Check that there are no more missing coordinates before proceeding
-    assert data['latitude_longitude'].notnull().any(), "Missing coordinates must be geocoded."
+    if data['latitude_longitude'].notnull().any(): 
+        raise AssertionError("Missing coordinates must be geocoded.")
 
     # Split latitude_longitude into separate columns and convert to float values: latitude, longitude
     if ['latitude', 'longitude'] not in data.columns.tolist():
@@ -44,49 +43,13 @@ def split_lat_long(data):
 
         # Add to original data
         return pd.concat([data, lat_long_series], axis=1)
-
-
-def main():
     
-    # Connect to db
-    conn = connect_db()
+    else:
+        return None
 
-    # SQL query to extract partial dataset
-    sql = 'SELECT * FROM {} LIMIT 500;'.format(DB_TABLE)
-
-    # Fetch data
-    data = fetch_data(sql, conn)
-    #print(data.head())
-
-    # Concatenate and create full_address
-    data = create_column_full_address(data)
-    #print(data['full_address'])
-
-    # Geocode
-    data = geocode_latitude_longitude(data)
-
-    # Split latitude_longitude into separate columns and convert to float values: latitude, longitude
-    data = split_column_lat_long(data)
-    print("\nNew columns created:\n\n", data[['latitude_longitude', 'latitude', 'longitude', 'full_address']].head(), "\n")
-
-    # Path to csv
-    save_path = project_dir + '/data/interim/permits_geocoded.csv'
-
-    # Save to interim folder
-    print("Saving to interim folder...")
-    save_csv(data, save_path)
-
-    conn.close()
-    print("Connection closed.")
-
-    return
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    # only works in Python 3.6.1 and above
+    
     # Get project root directory
     project_dir = sys.path[0]
 
@@ -100,13 +63,4 @@ if __name__ == '__main__':
     DB_HOST = os.getenv("DB_HOST")
     DATA_URL = os.getenv("DATA_URL")
 
-    # Google Maps environment variables
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-    GOOGLE_AGENT = "permits-data"
-    
-
-    # Environment variables specific to notebook
-    DATA_DIR = project_dir + '/data'
-    DB_TABLE = "permits_raw"
-
-    main()
+    #main()
